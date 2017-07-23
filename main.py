4e775e2209432d038d005c4f12e4ab0b2335858a
kevinlyu <kevinlumail@gmail.com>
2017-07-23 14:23:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from BatchProcess import *
+import matplotlib.pyplot as plt
+
+
+m = BatchMaker(input_dir="./testSet/", batch_size=4000)
+m.process_with_reshape(reshape=(100, 100))
+
+r = BatchReader()
+data = r.get_batch(selected_batch=[1])
+
+plt.imshow(data[10])
+plt.show()
